ab2dd263ed49672bfb00333d85a206c5594a70f7
inkfin <inkfinite@qq.com>
2024-02-21 19:22:28 -0500

    move shaders to shader folder

--- example_fish/src/lib.rs
+++ example_fish/src/lib.rs
@@ -318,7 +318,7 @@ impl State {
         // init shader
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+            source: wgpu::ShaderSource::Wgsl(include_str!("../shader/shader.wgsl").into()),
         });
 
         // or use this macro:
